27114bf8e3073d9d080236e8279cea3faee2a67d
Patelpriyasunil <patel.priyasunil@gmail.com>
2019-02-23 12:38:50 -0500

    updated files

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ var PORT = process.env.PORT || 8000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("public"));
+
 
 
 // ================================================================================
