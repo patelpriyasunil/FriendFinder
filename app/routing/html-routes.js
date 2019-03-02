336e557994474eed702618c5b9fe2bbb5d6fd661
Patelpriyasunil <patel.priyasunil@gmail.com>
2019-03-02 14:17:52 -0500

    final

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -20,7 +20,7 @@ module.exports = function(app){
 	});
 
 	// If no matching route is found default to home
-	app.get("/home", function(req, res){
+	app.get("/", function(req, res){
 		res.sendFile(path.join(__dirname, "../public/home.html"));
 	});
 	app.get("*", function(req, res){
